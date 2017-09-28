b71cae4d76d17c18a05c32eabc823ba924305960
Raychan <davidrueckert@hotmail.de>
2017-09-28 15:50:36 +0200

    Code Optimize

--- flag/config.cpp
+++ flag/config.cpp
@@ -4,31 +4,13 @@ class CfgPatches
 	{
 		units[] = {"SHD_FlagCarrierEU"};
 		weapons[] = {};
-		requiredVersion = 1.0;
+		requiredVersion = 0.1;
 		requiredAddons[] = {};
+		author="[SHD] Spearhead Division";
+		authors[] = { "Raychan", "HighPing"};
+		authorURL = "https://github.com/Spearhead-Division/";
 	};
 };
-class CfgFactionClasses
-{
-	class SHD_Flag
-	{
-		icon = "\SHD_Mod\flag\data\shd_pinup_512x256.paa";
-		displayName = "Fahne (SHD)";
-		priority = 5;
-		side = 1;
-	};
-};
-class CfgVehicles
-{
-	class SHD_FlagCarrierEU;
-	class SHD_Flag: SHD_FlagCarrierEU
-	{
-		scope = 2;
-		accuarcy = 1000;
-		displayName = "Fahne (SHD)";
-		class EventHandlers
-		{
-			init = "(_this select 0) setFlagTexture ""SHD_Mod\flag\data\shd_pinup_512x256.paa""";
-		};
-	};
-};
+
+#include "CfgFactionClasses.hpp"
+#include "CfgVehicles.hpp"
