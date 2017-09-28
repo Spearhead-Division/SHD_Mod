8805ec0c6ea34956c45d91727bf70d62f4dad252
Raychan <davidrueckert@hotmail.de>
2017-09-28 16:11:18 +0200

    Update CfgVehicles.hpp

--- flag/CfgVehicles.hpp
+++ flag/CfgVehicles.hpp
@@ -5,7 +5,7 @@ class CfgVehicles
 	{
 		scope = 2;
 		accuarcy = 1000;
-		displayName = "Fahne (SHD)";
+		displayName = STR_SHD_flag_DisplayName;
 		class EventHandlers
 		{
 			init = "(_this select 0) setFlagTexture ""SHD_Mod\flag\data\shd_pinup_512x256.paa""";
