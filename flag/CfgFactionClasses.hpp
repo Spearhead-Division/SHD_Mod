487f7d5f517abb62be2048e1cdcd2b3687375d50
Raychan <davidrueckert@hotmail.de>
2017-09-28 16:02:39 +0200

    Update CfgFactionClasses.hpp

--- flag/CfgFactionClasses.hpp
+++ flag/CfgFactionClasses.hpp
@@ -3,7 +3,7 @@ class CfgFactionClasses
 	class SHD_Flag
 	{
 		icon = "\SHD_Mod\flag\data\shd_pinup_512x256.paa";
-		displayName = "Fahne (SHD)";
+		displayName = STR_SHD_flag_DisplayName;
 		priority = 5;
 		side = 1;
 	};
